5cf219ed890dc262520d0b3c58c4f483d2bc331d
Matt Reeping <bowzer92@aol.com>
2013-12-07 19:03:39 -0500

    Set up src and stuff. Only added Wumpus driver
    Only has print statement stating its the wumpus driver

---
+++ src/wumpusAgent/WumpusDriver.java
@@ -0,0 +1,15 @@
+package wumpusAgent;
+
+/**
+ * This class represents the Wumpus World Agent's driver class
+ *  ***Don't really know where to go with it right now.
+ * @author Matt Reeping & John Wright
+ * @version 12/7/2013
+ */
+public class WumpusDriver {
+
+	
+	public static void main(String[] args){
+		System.out.println("This is going to be the Wumpus Driver!!!");
+	}
+}
