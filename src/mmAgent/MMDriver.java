6abb6b160cc65d6211bc0d8ab2f172d5f60db939
Matt Reeping <bowzer92@aol.com>
2013-12-07 19:01:42 -0500

    Setting up the src and other stuff. Only placed driver

---
+++ src/mmAgent/MMDriver.java
@@ -0,0 +1,15 @@
+package mmAgent;
+
+/**
+ * This class represents the MasterMind driver
+ *  *** Don't know where to go with it yet.
+ * @author Matt Reeping & John Wright
+ * @version 12/7/2013
+ */
+public class MMDriver {
+
+	
+	public static void main(String[] args){
+		System.out.println("This is the Master Mind driver!!!");
+	}
+}
